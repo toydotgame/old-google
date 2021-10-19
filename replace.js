9d9203e5b4812e3e170ff8f0735311ebd8e53203
toydotgame <toydotgame@gmail.com>
2021-10-19 19:08:55 +1000

    Added full functionality for `/` and `/search` pages

--- replace.js
+++ replace.js
@@ -1,7 +1,18 @@
 var imgclass = "lnXdpd";
+var imgpos = 0;
 var replacementurl = "https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png";
 
-var img = document.getElementsByClassName(imgclass)[0];
-img.srcset = ""; // TODO: Remove the `srcset` tag entirely. (For now we'll set it to a null value)
-img.src = replacementurl;
-
+var img;
+var page = location.pathname;
+if(page == "/" || page == "/webhp") {
+	console.log("[Old Google] Page is at root. Looking for `img.lnXdpd` and replacing.");
+	img = document.getElementsByClassName(imgclass)[0];
+	img.srcset = ""; // TODO: Remove the `srcset` tag entirely. (For now we'll set it to a null value)
+	img.src = replacementurl;
+} else if(page == "/search") {
+	console.log("[Old Google] Page is at `/search`. Looking for `img[" + imgpos + "]` and replacing.");
+	img = document.getElementsByTagName("img")[imgpos];
+	img.src = replacementurl;
+} else {
+	console.log("[Old Google] Page is not known. Will not replace any images.");
+}
