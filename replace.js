671c76a154df0dc02e89da300dacc3ad158029e1
toydotgame <toydotgame@gmail.com>
2021-10-19 19:46:17 +1000

    Disabled some code temporarily

--- replace.js
+++ replace.js
@@ -1,12 +1,12 @@
 var imgclass = "lnXdpd";
 var imgpos = 0;
 var replacementurl = chrome.extension.getURL('resources/logo.png');
-var favicon = chrome.extension.getURL('resources/favicon.ico');
+//var favicon = chrome.extension.getURL('resources/favicon.ico');
 
 var img;
 var page = location.pathname;
 if(page == "/" || page == "/webhp") {
-	console.log("[Old Google] Page is at root. Looking for `img.lnXdpd` and replacing.");
+	console.log("[Old Google] Page is at root. Looking for `img." + imgclass + "` and replacing.");
 	img = document.getElementsByClassName(imgclass)[0];
 	img.srcset = ""; // TODO: Remove the `srcset` tag entirely. (For now we'll set it to a null value)
 	img.src = replacementurl;
@@ -18,10 +18,10 @@ if(page == "/" || page == "/webhp") {
 	console.log("[Old Google] Page is not known. Will not replace any images.");
 }
 
-var link = document.querySelector("link[rel~='icon']");
+/*var link = document.querySelector("link[rel~='icon']");
 if (!link) {
 	link = document.createElement('link');
 	link.rel = 'icon';
 	document.getElementsByTagName('head')[0].appendChild(link);
 }
-link.href = favicon;
+link.href = favicon;*/
