b2f358857c246fb9af83340a85eaa1ce44a1a031
toydotgame <toydotgame@gmail.com>
2021-10-19 18:15:51 +1000

    Added main functionality (for the homepage only, so far)

--- replace.js
+++ replace.js
@@ -1,1 +1,7 @@
-document.body.style.border = "10px solid red";
+var imgclass = "lnXdpd";
+var replacementurl = "https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png";
+
+var img = document.getElementsByClassName(imgclass)[0];
+img.srcset = ""; // TODO: Remove the `srcset` tag entirely. (For now we'll set it to a null value)
+img.src = replacementurl;
+
