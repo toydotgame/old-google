e57fb14806f8a57991bc7f538b3b0c770ec71227
toydotgame <toydotgame@gmail.com>
2021-10-21 13:31:34 +1000

    Removed Google Images support; Updated TODO; Added in reference to other extension logo; Rewrote comments; Removed unneeded `var` declarations

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Replaces the logo on `google.com` and on the Google search page to that of [the logo they had from 2010 to 2013](https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png), and it also changes the favicon to that of [the 2012 one](https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Google_Icon_%282010-2015%29.svg/512px-Google_Icon_%282010-2015%29.svg.png).
 
 ## Goals
-* [x] Make boilerplate code at least somewhat easily update-able.
 * [x] Implement favicon replacement.
-* [x] Allow extension to only run on `www.google` and `images.google` subdomains, just in case other subdomains use similar IDs and the code is run on the wrong images unintentionally (which could potentially cause graphical errors in webpage rendering).
+* [x] Allow extension to only run on `www.google` subdomain, just in case other subdomains use similar IDs and the code is run on the wrong images unintentionally (which could potentially cause graphical errors in webpage rendering).
 * [ ] Sign, package, and distribute this extension on [AMO (`addons.mozilla.org`)](https://addons.mozilla.org/).
+* [ ] Google Images support.
+* [ ] Add proper minimum version number \[based on `replace.js` code\] to manifest and to this README. 
 
 ## Compiling for Testing
 ### Requirements:
--- replace.js
+++ replace.js
@@ -1,22 +1,15 @@
 var imgclass = "lnXdpd";
-var imgpos = 0;
 var replacementurl = chrome.extension.getURL('resources/logo.png');
 var favicon = chrome.extension.getURL('resources/favicon.ico');
 
-var img;
-var page = location.pathname;
+var page = location.pathname; // Putting `location.pathname` in the `if()` causes a reload. Declaring it seperately does not cause a reload.
 if(page == "/" || page == "/webhp") {
-	console.log("[Old Google] Page is at root. Looking for `img." + imgclass + "` and replacing.");
-	img = document.getElementsByClassName(imgclass)[0];
-	img.srcset = ""; // TODO: Remove the `srcset` tag entirely. (For now we'll set it to a null value)
+	var img = document.getElementsByClassName(imgclass)[0];
+	img.srcset = "";
 	img.src = replacementurl;
-} else if(page == "/search") {
-	console.log("[Old Google] Page is at `/search`. Looking for `img[" + imgpos + "]` and replacing.");
-	img = document.getElementsByTagName("img")[imgpos];
+} else if(page == "/search") { // Seperate code needs to be run for the search page, as the image is used in a completely different way by Google.
+	img = document.getElementsByTagName("img")[0];
 	img.src = replacementurl;
-} else {
-	console.log("[Old Google] Page is not known. Will not replace any images.");
 }
 
-document.getElementsByTagName("head")[0].innerHTML += '<link rel="icon" href="' + favicon + '">'; // Although this looks like boilerplate code, it isn't. There's only ever one `<head>` element. `.innerHTML +=` appends the text inside the element.
-
+document.getElementsByTagName("head")[0].innerHTML += '<link rel="icon" href="' + favicon + '">'; // Appends a HTML-based favicon to the `<head>` element.
