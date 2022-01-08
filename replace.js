abf487c7cbe4dea4c651e318f84047a5395254b2
toydotgame <toydotgame@gmail.com>
2022-01-08 11:07:36 +1000

    Addressed issue #2 Currently just using boilerplate to replace the source of a div to _visually_ change the Google logo if there's a Doodle on the homepage.
    Clicking the logo whilst there's a Doodle active will cause the Doodle video/game popup to appear, as if the logo was as normal. (Hover title is also determined by the Doodle)

--- replace.js
+++ replace.js
@@ -1,13 +1,19 @@
 var imgclass = "lnXdpd";
 var headerbgclass = "rISBZc";
+var homedoodleclass = "ddlsv-svCta_";
 var replacementurl = chrome.extension.getURL('resources/logo.png');
 var favicon = chrome.extension.getURL('resources/favicon.ico');
 
 var page = location.pathname; // Putting `location.pathname` in the `if()` causes a reload. Declaring it seperately does not cause a reload.
 if(page == "/" || page == "/webhp") {
 	var img = document.getElementsByClassName(imgclass)[0];
-	img.srcset = "";
-	img.src = replacementurl;
+	if(img == null) {
+		img = document.getElementsByClassName(homedoodleclass)[0];
+		img.style.background = "url(\"" + replacementurl + "\") no-repeat scroll 0% 0%";
+	} else {
+		img.srcset = "";
+		img.src = replacementurl;
+	}
 } else if(page == "/search") { // Seperate code needs to be run for the search page, as the image is used in a completely different way by Google.
 	var headerbg = document.querySelectorAll('.' + headerbgclass + ':not(#dimg_1)')[0];
 	if(headerbg != null) {
