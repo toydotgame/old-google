03388b6f2db1782d53af57b448738283cd01f4fc
toydotgame <toydotgame@gmail.com>
2023-05-21 14:55:23 +1000

    Refactor
    - Optimised file layout
    - Runs on document_end
    - Google Images compatibility
    - Issue #3 fixed
    - No other buttons or divs are affected anymore
    - Proper support for SVG logo(s)

--- replace.js
+++ replace.js
@@ -1,27 +1,75 @@
-var imgclass = "lnXdpd";
-var headerbgclass = "rISBZc";
-var homedoodleclass = "ddlsv-svCta_";
-var replacementurl = chrome.extension.getURL('resources/logo.png');
+/*
+ * CREATED: 2021-10-19
+ * AUTHOR: toydotgame
+ * Main replacement script that handles boilerplate classes.
+ */
+
+var logourl = chrome.extension.getURL('resources/logo.png');
 var favicon = chrome.extension.getURL('resources/favicon.ico');
 
-var page = location.pathname; // Putting `location.pathname` in the `if()` causes a reload. Declaring it seperately does not cause a reload.
-if(page == "/" || page == "/webhp") {
-	var img = document.getElementsByClassName(imgclass)[0];
-	if(img == null) {
-		img = document.getElementsByClassName(homedoodleclass)[0];
-		img.style.background = "url(\"" + replacementurl + "\") no-repeat scroll 0% 0%";
-	} else {
-		img.srcset = "";
-		img.src = replacementurl;
-	}
-} else if(page == "/search") { // Seperate code needs to be run for the search page, as the image is used in a completely different way by Google.
-	var headerbg = document.querySelectorAll('.' + headerbgclass + ':not(#dimg_1)')[0];
-	if(headerbg != null) {
-		headerbg.remove();
-	}
+var hplogo = "lnXdpd";
+var gschlogo = "jfN4p";
+var ischlogo = "TYpZOd";
+var hplogodiv = "k1zIA";
+var doodlestyle = "LLD4me";
+var sharediv = "SuUcIb";
 
-	var img = document.getElementsByTagName("img")[0];
-	img.src = replacementurl;
+document.getElementsByTagName("head")[0].innerHTML += '<link rel="icon" href="' + favicon + '">';
+
+var subdomain = window.location.host.split('.')[0];
+var page = location.pathname;
+var isch = false;
+if(new URLSearchParams(window.location.search).get('tbm') == "isch") { // Query string `&tbm=isch` only present on Images results.
+	isch = true;
 }
 
-document.getElementsByTagName("head")[0].innerHTML += '<link rel="icon" href="' + favicon + '">'; // Appends a HTML-based favicon to the `<head>` element.
+switch(page) {
+	case "/":
+	case "/webhp":
+	case "/imghp":
+		HpLogoSwap();
+		break;
+	case "/search":
+		SchLogoSwap();
+	
+}
+
+/*
+ * void HpLogoSwap()
+ * Replaces Google Doodles and the regular logo image with the old logo on the Google homepages.
+ * Throws TypeError (on non-Doodle days) as it tries to replace Doodles without validation.
+ */
+function HpLogoSwap() {
+	console.log("hplogo");
+	if(!(page == "/imghp" || subdomain == "images")) {
+		document.getElementsByClassName(hplogodiv)[0].outerHTML = '<img class="' + hplogo + '">';
+	}
+	
+	document.getElementsByClassName(hplogo)[0].src = logourl;
+	document.getElementsByClassName(hplogo)[0].srcset = ""; // Clear override.
+	// Override Doodle size styles:
+	document.getElementsByClassName(doodlestyle)[0].classList.remove(doodlestyle);
+	document.getElementsByClassName(hplogo)[0].width = "272";
+	document.getElementsByClassName(hplogo)[0].height = "92";
+	// Remove Doodle share button:
+	document.getElementsByClassName(sharediv)[0].remove();
+}
+
+/*
+ * void SchLogoSwap()
+ * Replaces small logo on search pages.
+ * TODO: Doodle compatibility.
+ */
+function SchLogoSwap() {
+	console.log("splogo")
+	if(!isch) {
+		document.getElementsByClassName(gschlogo)[0].src = logourl;
+		return;
+	}
+	
+	console.log("isch running");
+	document.getElementsByClassName(ischlogo)[0].outerHTML = document.getElementsByClassName(ischlogo)[0].outerHTML.replace(/svg/g, "img");
+	document.getElementsByClassName(ischlogo)[0].height = "30"; // SVG proportions are 34px for some reason so override here.
+	document.getElementsByClassName(ischlogo)[0].src = logourl;
+}
+
