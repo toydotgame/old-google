2e8738a614dfa4cab3a89350bb1b1cef404bb3b2
toydotgame <toydotgame@gmail.com>
2023-08-26 23:14:31 +1000

    ion even know

--- ua.js
+++ ua.js
@@ -1,26 +1,25 @@
-chrome.webNavigation.onBeforeNavigate.addListener(
-  function(details) {
-    if (isRootPage(details.url)) {
       chrome.webRequest.onBeforeSendHeaders.addListener(
         function(details) {
-          for (var i = 0; i < details.requestHeaders.length; ++i) {
-            if (details.requestHeaders[i].name === "User-Agent") {
+	if(isRootPage(details.url) {
+          for (var i = 0; i < details.requestHeaders.length; i++) {
+            if (details.requestHeaders[i].name.toLowerCase() === "user-agent") {
               details.requestHeaders[i].value = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-              break;
             }
           }
           return { requestHeaders: details.requestHeaders };
-        },
+        }
+	},
         { urls: [details.url] },
         ["blocking", "requestHeaders"]
       );
-    }
-  }
-);
 
 function isRootPage(url) {
   var urlObj = new URL(url);
-  // The location.pathname.split() stability option is unavailable here. Fallback:
-  return urlObj.pathname === "/" || urlObj.pathname === "/webhp" || urlObj.pathname === "/imghp";
+//  return urlObj.pathname === "/" || urlObj.pathname === "/webhp" || urlObj.pathname === "/imghp";
+replace = urlObj.pathname === "/" || urlObj.pathname === "/webhp" || urlObj.pathname === "/imghp";
+//search = urlObj.pathname === "/search";
+alert("pathname: " + urlObj.pathname + ", pathname (split): " + urlObj.pathname.split('/')[1] + ", replacement: " + replace);
+return replace;
 }
+
 
