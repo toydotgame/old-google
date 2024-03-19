bdcef0eb32e1ad63e4f17b256a107a6c95c14907
toydotgame <toydotgame@gmail.com>
2024-03-19 16:31:45 +1000

    Changed variable names

--- replace.js
+++ replace.js
@@ -4,103 +4,100 @@
  * Main replacement script that handles boilerplate classes
  */
 
-var logourl = "https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png";
+var logoUrl = "https://upload.wikimedia.org/wikipedia/commons/3/3e/Google_2011_logo.png";
 var favicon = "https://www.w3schools.com/favicon.ico";
-//var logourl = chrome.extension.getURL('resources/logo.png');
+//var logoUrl = chrome.extension.getURL('resources/logo.png');
 //var favicon = chrome.extension.getURL('resources/favicon.ico');
 
-var hplogo = "lnXdpd";
-var gschlogo = "jfN4p";
-var ischlogo = "TYpZOd";
-var hplogodiv = "k1zIA";
-var sharediv = "SuUcIb";
-var randrow = ["zp6Lyf", "XtQzZd"]; // Classes of actual button div and then the navbar which is left too high
+var homepageLogo = ["lnXdpd", "k1zIA", "SuUcIb"]; // Homepage logo, its container, and the Doodle share button
+var searchLogo = ["jfN4p", "TYpZOd"]; // PNG and SVG (respectively) results page logos
+var randRow = ["zp6Lyf", "XtQzZd"]; // Classes of actual button div and then the navbar which is left too high
 
-RunWhenReady([document.getElementsByTagName("head")[0]], function(objects) {
-	objects[0].innerHTML += '<link rel="icon" href="' + favicon + '">';
+RunWhenReady([document.getElementsByTagName("head")[0]], function(elements) {
+	elements[0].innerHTML += '<link rel="icon" href="' + favicon + '">';
 });
 
 var subdomain = window.location.host.split('.')[0];
-var page = "/" + location.pathname.split('/')[1]; // Get root-most page in a backwards-compatible-with-`location.pathname` fashion
-var isch = false;
-if(new URLSearchParams(window.location.search).get('tbm') == "isch") { // Query string `&tbm=isch` only present on Images results
-	isch = true;
+var page = "/" + location.pathname.split('/')[1];
+var isImageSearch = false;
+if(new URLSearchParams(window.location.search).get('tbm') == "isch") { // Query string `&tbm=isch` only present on Images results, where the logo is an SVG
+	isImageSearch = true;
 }
 
 RunWhenReady([
-	document.getElementsByClassName(hplogodiv)[0],
-	document.getElementsByClassName(gschlogo)[0],
-	document.getElementsByClassName(ischlogo)[0]
-], function(objects) {
+	document.getElementsByClassName(homepageLogo[1])[0],
+	document.getElementsByClassName(searchLogo[0])[0],
+	document.getElementsByClassName(searchLogo[1])[0]
+], function(elements) {
 	Main();
 });
 
 /*
  * void Main()
- * Only called once one of the logo divs is detected to have been loaded into DOM
+ * Calls either function depending on what page is loaded right now
  */
 function Main() {
 	switch(page) {
 		case "/":
 		case "/webhp":
 		case "/imghp":
-			HpLogoSwap();
+			SwapHomepageLogo();
 			break;
 		case "/search":
-			SchLogoSwap();
+			SwapSearchLogo();
 	}
 }
 
 /*
- * void HpLogoSwap()
+ * void SwapHomepageLogo()
  * Replaces Google Doodles and the regular logo image with the old logo on the Google homepages
  */
-function HpLogoSwap() {
+function SwapHomepageLogo() {
 	if(!(page == "/imghp" || subdomain == "images")) {
-		document.getElementsByClassName(hplogodiv)[0].outerHTML = '<div style="margin-top:auto; max-height:92px;"><img class="' + hplogo + '"></div>';
+		document.getElementsByClassName(homepageLogo[1])[0].outerHTML = '<div style="margin-top:auto; max-height:92px;"><img class="' + homepageLogo[0] + '"></div>';
 	}
 
-	document.getElementsByClassName(hplogo)[0].src = logourl;
-	document.getElementsByClassName(hplogo)[0].srcset = ""; // Clear override
+	document.getElementsByClassName(homepageLogo[0])[0].src = logoUrl;
+	document.getElementsByClassName(homepageLogo[0])[0].srcset = ""; // Clear override
 	// Override Doodle size styles:
-	document.getElementsByClassName(hplogo)[0].width = "272";
-	document.getElementsByClassName(hplogo)[0].height = "92";
+	document.getElementsByClassName(homepageLogo[0])[0].width = "272";
+	document.getElementsByClassName(homepageLogo[0])[0].height = "92";
 	// Remove share button (only present on Doodles):
 	try {
-		document.getElementsByClassName(sharediv)[0].remove();
+		document.getElementsByClassName(homepageLogo[2])[0].remove();
 	} catch(TypeError) {}
 }
 
 /*
- * void SchLogoSwap()
+ * void SwapSearchLogo()
  * Replaces small logo on search pages
  * TODO: Doodle compatibility
  */
-function SchLogoSwap() {
-	if(!isch) {
-		document.getElementsByClassName(gschlogo)[0].src = logourl;
+function SwapSearchLogo() {
+	if(!isImageSearch) {
+		document.getElementsByClassName(searchLogo[0])[0].src = logoUrl;
 		return;
 	}
 	
-	document.getElementsByClassName(ischlogo)[0].outerHTML = document.getElementsByClassName(ischlogo)[0].outerHTML.replace(/svg/g, "img");
-	document.getElementsByClassName(ischlogo)[0].height = "30"; // SVG proportions are 34px for some reason so override here
-	document.getElementsByClassName(ischlogo)[0].src = logourl;
+	document.getElementsByClassName(searchLogo[1])[0].outerHTML = document.getElementsByClassName(searchLogo[1])[0].outerHTML.replace(/svg/g, "img");
+	document.getElementsByClassName(searchLogo[1])[0].height = "30"; // SVG proportions are 34px for some reason so override here
+	document.getElementsByClassName(searchLogo[1])[0].src = logoUrl;
 
 	// Remove random bar of buttons Google thought'd be a good idea to add:
-	document.getElementsByClassName(randrow[0])[0].remove();
-	document.getElementsByClassName(randrow[1])[0].style.height = "57px";
+	document.getElementsByClassName(randRow[0])[0].remove();
+	document.getElementsByClassName(randRow[1])[0].style.height = "57px";
 }
 
 /*
- * void RunWhenReady(Object[] objects, function code)
+ * void RunWhenReady(Object[] elements, function code)
  * Takes code and runs it when at least one of the input DOM elements loads.
  * TODO: Asynchronously create a new thread to run this code.
  */
-function RunWhenReady(objects, code) {
+function RunWhenReady(elements, code) {
 	console.log(code);
 	var observer = new MutationObserver(function (mutations, mutationInstance) {
 		if (objects.some(obj => obj)) {
-			code(objects);
+			code(elements);
 			mutationInstance.disconnect();
 		}
 	});
